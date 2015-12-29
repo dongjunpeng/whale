85fb3060e55f1a250d578fea856666193988ee1a
dongjunpeng <dongjunpeng0918@gmail.com>
2015-12-29 15:19:41 +0800

    添加EO和repository

---
+++ src/main/java/com/buterfleoge/whale/dao/InformationRepository.java
@@ -0,0 +1,9 @@
+package com.buterfleoge.whale.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.buterfleoge.whale.eo.Information;
+
+public interface InformationRepository extends CrudRepository<Information, Long> {
+	Information findByUserId(Long userId);
+}
