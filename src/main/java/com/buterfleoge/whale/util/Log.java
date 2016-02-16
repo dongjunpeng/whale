f8cee2b747c577a79c4b683b74f642c0a7c75986
zhenzong <zhenzong.peng@icloud.com>
2016-02-16 10:48:28 +0800

    merge

---
+++ src/main/java/com/buterfleoge/whale/util/Log.java
@@ -0,0 +1,16 @@
+package com.buterfleoge.whale.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 
+ *
+ * @author xiezhenzong
+ *
+ */
+public abstract class Log {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Log.class);
+
+}
