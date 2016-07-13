82811a53396b03505ff954c3889315e9f4b6f120
zhenzong <zhenzong.peng@icloud.com>
2016-07-14 00:27:49 +0800

    rsa -> aes

--- src/main/java/com/buterfleoge/whale/AesEncryption.java
+++ src/main/java/com/buterfleoge/whale/AesEncryption.java
@@ -3,120 +3,64 @@
  */
 package com.buterfleoge.whale;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.PrivateKey;
-import java.security.PublicKey;
+import java.security.Key;
 
 import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
+import org.apache.commons.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 /**
  * @author xiezhenzong
  *
  */
 @Component
-public class RsaEncryption implements InitializingBean {
+public class AesEncryption implements InitializingBean {
 
-    private static final Logger LOG = LoggerFactory.getLogger(RsaEncryption.class);
+    private static final Logger LOG = LoggerFactory.getLogger(AesEncryption.class);
 
-    private static final String ALGORITHM = "RSA";
+    private static final String ALGORITHM = "AES";
 
-    @Value("${rsa.private.key}")
-    private String privateKeyPath;
+    @Value("${aes.key}")
+    private String key;
 
-    @Value("${rsa.public.key}")
-    private String publicKeyPath;
-
-    private PrivateKey privateKey;
-
-    private PublicKey publicKey;
+    private Key spec;
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        File privateKeyFile = new File(privateKeyPath);
-        File publicKeyFile = new File(publicKeyPath);
-        if (!privateKeyFile.exists() || !publicKeyFile.exists()) {
-            generateKey(privateKeyFile, publicKeyFile);
-        } else {
-            privateKey = (PrivateKey) readObjectFromFile(privateKeyFile);
-            publicKey = (PublicKey) readObjectFromFile(publicKeyFile);
+        if (StringUtils.isEmpty(key)) {
+            throw new IllegalArgumentException("invalid value of aes.key configuration");
         }
+        spec = new SecretKeySpec(key.getBytes(), ALGORITHM);
     }
 
-    public byte[] encrypt(String text) {
-        byte[] cipherText = null;
+    public String encrypt(String text) {
         try {
             final Cipher cipher = Cipher.getInstance(ALGORITHM);
-            cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-            cipherText = cipher.doFinal(text.getBytes());
-            return cipherText;
+            cipher.init(Cipher.ENCRYPT_MODE, spec);
+            byte[] cipherText = cipher.doFinal(text.getBytes());
+            return Base64.encodeBase64String(cipherText);
         } catch (Exception e) {
             LOG.error("encrypt failed", e);
             return null;
         }
     }
 
-    public String decrypt(byte[] text) {
-        byte[] dectyptedText = null;
+    public String decrypt(String text) {
         try {
             final Cipher cipher = Cipher.getInstance(ALGORITHM);
-            cipher.init(Cipher.DECRYPT_MODE, privateKey);
-            dectyptedText = cipher.doFinal(text);
+            cipher.init(Cipher.DECRYPT_MODE, spec);
+            byte[] dectyptedText = cipher.doFinal(Base64.decodeBase64(text));
             return new String(dectyptedText);
         } catch (Exception e) {
             LOG.error("decrypt failed", e);
             return null;
-        }
-    }
-
-    private void generateKey(File privateKeyFile, File publicKeyFile) throws Exception {
-        final KeyPairGenerator keyGen = KeyPairGenerator.getInstance(ALGORITHM);
-        keyGen.initialize(1024);
-        final KeyPair key = keyGen.generateKeyPair();
-        if (privateKeyFile.exists()) {
-            privateKeyFile.delete();
-        }
-        if (publicKeyFile.exists()) {
-            publicKeyFile.delete();
-        }
-        privateKeyFile.createNewFile();
-        publicKeyFile.createNewFile();
-
-        privateKey = key.getPrivate();
-        publicKey = key.getPublic();
-        ObjectOutputStream privateKeyOS = new ObjectOutputStream(new FileOutputStream(privateKeyFile));
-        privateKeyOS.writeObject(privateKey);
-        privateKeyOS.close();
-        ObjectOutputStream publicKeyOS = new ObjectOutputStream(new FileOutputStream(publicKeyFile));
-        publicKeyOS.writeObject(publicKey);
-        publicKeyOS.close();
-    }
-
-    private Object readObjectFromFile(File file) throws Exception {
-        ObjectInputStream inputStream = null;
-        try {
-            inputStream = new ObjectInputStream(new FileInputStream(file));
-            return inputStream.readObject();
-        } finally {
-            if (inputStream != null) {
-                try {
-                    inputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
         }
     }
 
